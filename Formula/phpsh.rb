ec3731304f36490eb54c3f8df7a9416f7397c050
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2014-07-21 16:36:38 +0300

    fix phpsh

--- Formula/phpsh.rb
+++ Formula/phpsh.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Phpsh < Formula
   homepage 'http://www.phpsh.org/'
-  url 'https://github.com/facebook/phpsh.git'
+  url 'https://github.com/facebookarchive/phpsh.git'
   version '1.3'
 
   depends_on 'python'
@@ -13,23 +13,4 @@ class Phpsh < Formula
     system 'rm', '-rf', "#{prefix}/man"
     bin.install ['src/phpsh']
   end
-
-  def patches
-    DATA
-  end
 end
-
-__END__
-diff --git a/src/__init__.py b/src/__init__.py
-index 31b3474..3d1d874 100644
---- a/src/__init__.py
-+++ b/src/__init__.py
-@@ -758,7 +758,7 @@ Type 'e' to open emacs or 'V' to open vim to %s: %s" %
-                 ret_code = self.p.poll()
-                 if debug:
-                     print "ret_code: " + str(ret_code)
--                if ret_code != None:
-+                if ret_code != None and ret_code != 0:
-                     if debug:
-                         print "NOOOOO"
-                     print "subprocess died with return code: " + repr(ret_code)
